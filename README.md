87c96291f7422f03e95fa4b5a7d4e830d7b6ded6
Jimmy Batuhan <jimmybatuhan@gmail.com>
2020-06-08 14:40:56 +0700

    fixed bugs on payment intent

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# paymongo-api
-Paymongo API Integration
+# Paymongo API Client
+
+A lightweight client-side API Integration for Paymongo.
+
+## Installation
+
+```bash
+npm install paymongo-api-client
+```
+## Environment Variables
+Must have the following variables declared in your application environment 
+
+```php
+#Paymongo version to use
+PAYMONGO_VERSION
+
+#Your account's Public Key
+PAYMONGO_PUBLIC_KEY
+
+#You account's Secret Key
+
+PAYMONGO_SECRET_KEY
+
+#Tells the package where this is test or live
+PAYMONGO_ENV
+```
+
+## Usage
+### Payment Intent
+
+```python
+import paymongo from 'paymongo-api-client'
+
+paymongo.PaymentIntent.create(attributes) # create a payment intent 
+paymongo.PaymentIntent.retrive(intentId, attributes) # retrieve a payment intent 
+paymongo.PaymentIntent.attachPaymentMethod(intentId, attributes) # attach a payment method to a payment intent
+```
+### Payment Method
+
+```python
+import paymongo from 'paymongo-api-client'
+
+paymongo.PaymentMethod.create(attributes) # create a payment method 
+paymongo.PaymentMethod.retrive(methodId) # retrieve a payment method 
+```
+### Payment
+```python
+import paymongo from 'paymongo-api-client'
+
+paymongo.Payment.create(attributes) # create a payment method 
+paymongo.Payment.retrive(paymentId) # retrieve a payment method 
+paymongo.Payment.list(params) # retrieve a payment method 
+```
+### Source
+
+```python
+import paymongo from 'paymongo-api-client'
+
+paymongo.Source.create(attributes) # create a payment method 
+paymongo.Source.retrive(sourceId) # retrieve a payment method 
+```
+### Webhook
+
+```python
+import paymongo from 'paymongo-api-client'
+
+paymongo.Webhook.create(attributes) # create a payment method 
+paymongo.Webhook.retrive(sourceId) # retrieve a payment method 
+paymongo.Webhook.list(attributes) # create a payment method 
+paymongo.Webhook.enable(sourceId) # retrieve a payment method 
+paymongo.Webhook.disable(sourceId) # retrieve a payment method 
+```
--- index.js
+++ index.js
@@ -9,34 +9,32 @@ const request = (endpoint, requestObj) => {
     return axios({
         url: `https://api.paymongo.com/${process.env.PAYMONGO_VERSION}/${endpoint}`,
         method: requestObj.method || "POST",
-        data: { data: { attributes: requestObj.body } }
-    }).catch(err => console.log({
-        headers: err.response.headers,
-        errors: err.response.data.errors
-    }));
+        data: { data: { attributes: requestObj.body || {} } }
+    }).catch(err => {
+        if(err) throw err;
+    });
 }
 
-class PaymentIntent {
+  class PaymentIntent {
 
     api_endpoint = `payment_intents`;
 
     create(attributes) {
         return request(this.api_endpoint, {
             body: attributes,
-            key: process.env.PAYMONGO_PUBLIC_KEY
+            key: process.env.PAYMONGO_SECRET_KEY
         });
     }
 
-    retrieve(intentId, attributes) {
+    retrieve(intentId) {
         return request(`${this.api_endpoint}/${intentId}`, {
             method: "GET",
-            body: attributes,
             key: process.env.PAYMONGO_PUBLIC_KEY
         });
     }
 
     attachPaymentMethod(intentId, attributes) {
-        return request(`${this.api_endpoint}/${intentId}`, {
+        return request(`${this.api_endpoint}/${intentId}/attach`, {
             body: attributes,
             key: process.env.PAYMONGO_SECRET_KEY
         });
@@ -50,14 +48,13 @@ class PaymentMethod {
     create(attributes) {
         return request(`${this.api_endpoint}`, {
             body: attributes,
-            key: process.env.PAYMONGO_SECRET_KEY
+            key: process.env.PAYMONGO_PUBLIC_KEY
         });
     }
 
-    retrieve(methodId, attributes) {
+    retrieve(methodId) {
         return request(`${this.api_endpoint}/${methodId}`, {
             method: "GET",
-            body: attributes,
             key: process.env.PAYMONGO_PUBLIC_KEY,
         });
     }
@@ -77,15 +74,13 @@ class Payments {
     retrieve(paymentId) {
         return request(`${this.api_endpoint}/${paymentId}`, {
             method: "GET",
-            body: attributes,
             key: process.env.PAYMONGO_PUBLIC_KEY,
         });
     }
 
     list(params) {
-        return request(`${this.api_endpoint}?${parms}`, {
+        return request(`${this.api_endpoint}?${params}`, {
             method: "GET",
-            body: null,
             key: process.env.PAYMONGO_PUBLIC_KEY
         });
     }
@@ -104,7 +99,6 @@ class Source {
     retrieve(sourceId) {
         return request(`${this.api_endpoint}/${sourceId}`, {
             method: "GET",
-            body: attributes,
             key: process.env.PAYMONGO_PUBLIC_KEY
         });
     }
@@ -124,7 +118,6 @@ class Webhook {
     list() {
         return request(`${this.api_endpoint}`, {
             method: "GET",
-            body: {},
             key: process.env.PAYMONGO_SECRET_KEY
         })
     }
@@ -132,37 +125,28 @@ class Webhook {
     retrieve(id) {
         return request(`${this.api_endpoint}/${id}`, {
             method: "GET",
-            body: {},
             key: process.env.PAYMONGO_SECRET_KEY
         })
     }
 
     disable(id) {
         return request(`${this.api_endpoint}/${id}/disable`, {
-            body: {},
             key: process.env.PAYMONGO_SECRET_KEY
         })
     }
 
-    enable() {
+    enable(id) {
         return request(`${this.api_endpoint}/${id}/enable`, {
-            body: {},
             key: process.env.PAYMONGO_SECRET_KEY
         })
     }
-
 }
 
 class Paymongo {
-
     Webhook = new Webhook();
-
     PaymentIntent = new PaymentIntent();
-
     PaymentMethod = new PaymentMethod();
-
     Payments = new Payments();
-
     Source = new Source();
 }
 
